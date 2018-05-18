457ffddd77515931512e0490d5ee352d09f450e0
ajonp <developer@ajonp.info>
2018-05-18 15:27:04 -0400

    all the codes

---
+++ src/app/app.module.ts
@@ -0,0 +1,30 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes, Route } from '@angular/router';
+import { IonicModule } from 'ionic-angular';
+
+import { AppComponent } from './app.component';
+import { MainRouteComponent } from './main-route/main-route.component';
+import { SampleRouteComponent } from './sample-route/sample-route.component';
+
+
+const routes = [
+  { path: '', component: MainRouteComponent },
+  { path: 'sampleRoute', component: SampleRouteComponent },
+];
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    SampleRouteComponent,
+    MainRouteComponent
+  ],
+  imports: [
+    BrowserModule,
+    RouterModule.forRoot(routes, { useHash: false }),
+    IonicModule.forRoot(AppComponent),
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/sample-route/sample-route.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { SampleRouteComponent } from './sample-route.component';
+
+describe('SampleRouteComponent', () => {
+  let component: SampleRouteComponent;
+  let fixture: ComponentFixture<SampleRouteComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ SampleRouteComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(SampleRouteComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
